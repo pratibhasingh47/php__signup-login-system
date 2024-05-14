9af78c46204359ce494c474c796bfd8e9e822904
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-14 21:26:55 +0530

    update.php

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -13,25 +13,40 @@ if ( $_SERVER["REQUEST_METHOD"] ==="POST" ){
         //ERROR HANDLER
         $errors = [];
 
-        if(is_input_empty($username,$pwd,$email)){
+        if(is_input_empty($username,$pwd)){
                 $errors["empty_input"] = "Fill in all fields";
         }
 
-        
+        $result = get_user( $pdo, $username);
+
+        if ( is_username_wrong($result)){
+            $errors["login_incorrect"] = "Incorrect login info...";
+        }
+
+        if ( is_username_wrong($result) && is_password_wrong($pwd,$result["pwd"]) ){
+            $errors["login_incorrect"] = "Incorrect login info...";
+        }
 
         require_once 'config_session.inc.php';
 
         if($errors){
             $_SESSION["error_signup"] = $errors;
             header("Location: ../index.php");
+            die();
         }
+        
+        $newSessionId = session_create_id();
+        $sessionId = $newSessionId . "_" . $result["id"];
+        session_id($sessionId);
 
-        create_user( $pdo, $pwd , $username , $email);
+        $_SESSION["user_id"] = $result["id"];
+        $_SESSION["user_username"] = htmlspecialchars($result["username"]);
+
+        $_SESSION["last_regeneration"] = time();
 
         header("Location: ../index.php?signup=success");
 
-        $pdo = null;
-        $stmt = null;
+        
     }
     catch( PDOException $e ){
         die("Query Failed: " . $e->getMessage());
--- includes/login_contr.inc.php
+++ includes/login_contr.inc.php
@@ -2,6 +2,15 @@
 
 declare(strict_types = 1)
 
+function is_input_empty(string $username,$pwd,$email){
+    if(empty($username) || empty($pwd) ){
+        return true;
+    }
+    else{
+        return false
+    }
+}
+
 function is_username_wrong( bool|array $result)
 {
     if ( !$result ){
